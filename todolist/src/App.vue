b3c43cc5c9296d9593568adb83cc54bc12fe0079
Vladimir <vova_denisov_2003@mail.ru>
2020-12-10 19:42:49 +0300

    vue todolist compleated

---
+++ todolist/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/todolist.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      todos: ['Зарядка', 'Завтрак', 'Проснуться', 'почистить зубы']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #a1e300;
+  }
+  #app {
+    width: 450px;
+    height: 350px;
+    margin: auto;
+    margin-top: 120px;
+    background-color: rgb(70, 86, 230);
+    padding: 40px;
+    border-radius: 15px;
+  }
+</style>
---
+++ todolist/src/components/todolist.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos" v-on:click="deleteTodo(index)" v-bind:key="index">{{ todo }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['todos'],
+    methods: {
+        deleteTodo(index){
+            this.$emit('delete-todo', index);
+        }
+    }
+}
+</script>
+
+<style>
+    li {
+        background-color: rgb(255, 92, 92);
+        color: white;
+        text-align: center;
+        margin: 5px;
+        padding: 20px;
+        font-size: 25px;
+        flex-flow: 'roboto';
+        border: black solid 1px;
+        border-radius: 12px;
+        list-style-type: none;
+        cursor: pointer;
+    }
+    ul {
+        margin-left: -40px;
+        /* padding-top: 20px; */
+    }
+</style>
